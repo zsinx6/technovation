b3f1e1d3c78913f82c6436d84fe8f22289251b6b
zSinx6 <lucassin_x@hotmail.com>
2020-03-14 01:12:59 -0300

    Add migration and change app name

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy dog'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 api = Api(app)
 
